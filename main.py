d214286bd405537b49c7163fead31ef366786e8f
JesusR209 <jesus092402.jr@gmail.com>
2024-04-30 21:21:14 -0700

    wordcount, lettercount, report function added

--- main.py
+++ main.py
@@ -24,7 +24,34 @@ def countLetters():
                     lettercounter[i] += 1
                 else: 
                     lettercounter[i] = 1
-                
-                
-    print(lettercounter)
 countLetters()
+
+def report():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        count = len(file_contents.split())
+    print("--- Begin report of books/frankenstein ---")
+    print(f"{count} words found in the document")
+    lettercounter = {}
+    lowercase = file_contents.lower()
+    for i in lowercase:
+            if i.isalpha():
+                if i in lettercounter:
+                    lettercounter[i] += 1
+                else: 
+                    lettercounter[i] = 1
+    
+    list_of_dictionaries = []
+    for char, count in lettercounter.items():
+         entry = {"char": char, "count": count}
+         list_of_dictionaries.append(entry)
+
+    list_of_dictionaries.sort(key=lambda x: x['count'], reverse=True)
+
+    for i in list_of_dictionaries:
+        print(f"The '{i['char']}' character was found {i['count']} times")
+    
+    
+    print("\n --- End report ---")
+
+report()
